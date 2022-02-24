e69ad992e4f515a7908a8e59cf0abc940c0360e2
fulsep <57238803+fulsep@users.noreply.github.com>
2022-02-24 15:12:35 +0700

    feat: search and filter

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import Homepage from './pages/Homepage'
 import ListItem from './pages/ListItem'
 import Features from './pages/Features'
 import Pricing from './pages/Pricing'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { unstable_HistoryRouter as HistoryRouter,BrowserRouter, Route, Routes } from 'react-router-dom'
 import DetailItem from './pages/DetailItem'
 import CharacterList from './pages/CharacterList'
+import CharacterDetail from './pages/CharacterDetail'
+import NotFound from './pages/NotFound'
+import { createBrowserHistory } from 'history'
 
 export default class App extends Component {
   componentDidMount(){
     console.log(this.props)
   }
+  history = createBrowserHistory()
   render() {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Homepage />}/>
-          <Route path='list' element={<ListItem />} />
-          <Route path='vehicles' element={<ListItem />} />
-          <Route path='vehicles/:id' element={<DetailItem />} />
-          <Route path='features' element={<Features />} />
-          <Route path='pricing' element={<Pricing />} />
-          <Route path='characters' element={<CharacterList />} />
-        </Routes>
-      </BrowserRouter>
+      <HistoryRouter history={this.history}>
+        {/* <BrowserRouter> */}
+          <Routes>
+            <Route path='/' element={<Homepage />}/>
+            <Route path='404' element={<NotFound />} />
+            <Route path='list' element={<ListItem />} />
+            <Route path='vehicles' element={<ListItem />} />
+            <Route path='vehicles/:id' element={<DetailItem />} />
+            <Route path='features' element={<Features />} />
+            <Route path='pricing' element={<Pricing />} />
+            <Route path='characters' element={<CharacterList />} />
+            <Route path='characters/:id' element={<CharacterDetail history={this.history} />} />
+          </Routes>
+        {/* </BrowserRouter> */}
+      </HistoryRouter>
     )
   }
 }
